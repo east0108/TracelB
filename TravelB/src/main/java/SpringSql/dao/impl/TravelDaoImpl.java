a280ff7f935520cc178c3dc78c1e4a695eb2afb9
User <User@DESKTOP-9TQJKTB>
2023-01-17 22:33:59 +0800

    1

--- TravelB/src/main/java/SpringSql/dao/impl/TravelDaoImpl.java
+++ TravelB/src/main/java/SpringSql/dao/impl/TravelDaoImpl.java
@@ -20,7 +20,7 @@ public class TravelDaoImpl implements TravelDao {
 	@Override
 	public List<Travel> getTravelById(String travelId) {
 		try {
-			String sql= "SELECT id,name,town,address,tel,tickets,introduce,picture FROM travel2 WHERE town = :travelId";
+			String sql= "SELECT id,name,town,address,tel,tickets,introduce,picture FROM travel2  WHERE town  = :travelId";
 			Map<String,Object> map=new HashMap<>();
 			
 			
