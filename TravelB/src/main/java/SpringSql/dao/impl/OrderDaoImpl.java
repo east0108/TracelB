2cb6ba413a2b037252d65803702709af340f01d6
User <User@DESKTOP-9TQJKTB>
2023-02-04 11:41:25 +0800

    123

--- TravelB/src/main/java/SpringSql/dao/impl/OrderDaoImpl.java
+++ TravelB/src/main/java/SpringSql/dao/impl/OrderDaoImpl.java
@@ -2,9 +2,11 @@ package SpringSql.dao.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import SpringSql.dao.OrderDao;
 
+@Component
 public class OrderDaoImpl implements OrderDao{
 	
 	@Autowired
