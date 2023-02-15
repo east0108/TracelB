d3971950f87353f2ac2caa3b852b5f3292adadf3
ChoBits77 <eagle724eagle724@gmail.com>
2023-02-15 10:21:32 +0800

    2/15

--- TravelB/src/main/resources/static/js/find.js
+++ TravelB/src/main/resources/static/js/find.js
@@ -260,35 +260,10 @@ function Info (data) {
             </div>
           
             <!-- CIC_Modal -->
-            <div class="modal fade" id="cart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">購物車</h5>
-
-                            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <table class="show-cart table">
-
-                            </table>
-                            <div>總額: NT<span class="total-cart"></span></div>
-
-                        </div>
-                        <div class="modal-footer">
-                            <div><button class="clear-cart btn btn-danger">清除購物車</button></div>
-                            <button onclick="myFunction(cartArray)" type="button" class="btn btn-primary">立即下定</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
+           
 
             </div>
+            
             </div>`
 
         $("#dataList").append(html);
